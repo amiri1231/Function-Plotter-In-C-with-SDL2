9bd66c4cacf7633e502fa1ea9e06f683524ec26e
amiri1231 <mohsenamiri42@gmail.com>
2025-12-26 12:57:47 -0800

    Added X and Y coordinates

--- plotter.c
+++ plotter.c
@@ -6,10 +6,6 @@
 #define WIDTH 900
 #define HEIGHT 600
 
-void draw_grid(SDL_Surface *psurface) 
-{
-    
-}
 
 void draw_at_grid_coordinate(SDL_Surface *psurface, SDL_Rect *prect, Uint32 color)
 {
@@ -20,21 +16,47 @@ void draw_at_grid_coordinate(SDL_Surface *psurface, SDL_Rect *prect, Uint32 colo
 
 }
 
+void draw_grid(SDL_Surface *psurface) 
+{
+    SDL_Rect x_axis;
+    x_axis.x = -WIDTH / 2;
+    x_axis.y = 0;
+    x_axis.w = WIDTH;
+    x_axis.h = 2;
+
+    SDL_Rect y_axis;
+    y_axis.x = 0;
+    y_axis.y = HEIGHT / 2;
+    y_axis.w = 2;
+    y_axis.h = HEIGHT;
+    draw_at_grid_coordinate(psurface, &x_axis, 0xFFFFFF);
+    draw_at_grid_coordinate(psurface, &y_axis, 0xFFFFFF);
+    
+}
+
 
 int main()
 {
     printf("Hello World!\n");
+    
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("SDL_Init Error: %s\n", SDL_GetError());
+    return 1;
+}
+    
     SDL_Window *pwindow = SDL_CreateWindow("Plotter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
+    
+
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
-    SDL_Rect rect = (SDL_Rect) {WIDTH/2, HEIGHT/2, 50, 50};
-    Uint32 color = 0xFF00FF00;
-    SDL_FillRect(psurface, &rect, color);
-    SDL_UpdateWindowSurface(pwindow);
-
-    draw_at_grid_coordinate(psurface, &rect, color);
+    SDL_Rect rect = (SDL_Rect) {50,50, 50, 50};
+    Uint32 color = 0xFF0000FF;
+ 
+    
     draw_grid(psurface);
-
+    draw_at_grid_coordinate(psurface, &rect, color);
+    
+    SDL_UpdateWindowSurface(pwindow);
 
     SDL_Event event;
     bool app_running = true;
@@ -46,6 +68,9 @@ int main()
 
     }
 }
+
+SDL_Quit();
+return 0;
 
 
 }
